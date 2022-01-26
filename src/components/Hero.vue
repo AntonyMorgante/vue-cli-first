ab64044f88682ee5d7c713d6e2aa8c2d2bee5500
AntonyMorgante <antonymorgante1@gmail.com>
2022-01-26 17:43:02 +0100

    clis

---
+++ src/components/Hero.vue
@@ -0,0 +1,18 @@
+<template>
+  <section id="jumbo">JUMBO</section>
+</template>
+
+<script>
+export default {
+  name: 'Hero'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+#jumbo {
+    background-color: blue;
+    height: 200px;
+    width: 100vw;
+}
+</style>
---
+++ src/components/Title.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>La mia prima app con VUE CLI!</div>
+</template>
+
+<script>
+export default {
+  name: 'Title'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
